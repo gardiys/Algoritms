39eb46331a1c9050e68bf1d9d247b92a8a5b7635
Антон Щербак <Gardiys@MacBook-Air-Anton.local>
2020-09-16 22:40:39 +0300

    add binary search with counting

--- BinarySearch.py
+++ BinarySearch.py
@@ -11,6 +11,17 @@ def binary_search(k, a):
             r = m - 1
     return -1
 
+def binary_search_with_counting(k, a, last:bool):
+    n = len(k)
+    l, r = 0, len(a) - 1
+    while l <= r:
+        m = l + (r - l) // 2
+        if k > a[m] or last and k == a[m]:
+            l = m + 1
+        elif k < a[m] or (not last) and k == a[m]:
+            r = m - 1
+    return r if last else l
+
 
 def main():
     result = ""
@@ -19,6 +30,12 @@ def main():
     for i in b:
         print(binary_search(i, a), end=' ')
 
+def main2():
+    a = input()
+    pattern = input()
+    right = binary_search(pattern,a,True)
+    left = binary_search(pattern,a,False)
+    print(right - left + 1)
 
 if __name__ == "__main__":
     main()
