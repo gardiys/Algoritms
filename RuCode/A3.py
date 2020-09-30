24ecc4a25cc56fabd1513bebda46b4b3858a5b2b
Антон Щербак <Gardiys@Air-Anton.Dlink>
2020-10-01 01:04:04 +0300

    fix

--- RuCode/A3.py
+++
@@ -1,3 +0,0 @@
-x1,y1,x2,y2 = [int(i) for i in input().split()]
-
-print(((x1-x2)**2 + (y1-y2)**2)**0.5)
--- RuCode/B3.py
+++
@@ -1,8 +0,0 @@
-import math
-x1,y1,x2,y2 = [int(i) for i in input().split()]
-
-a = (x1**2 + y1**2) ** 0.5
-b = (x2**2 + y2**2) ** 0.5
-
-cos_a = (x1*x2 + y1*y2) / (a*b)
-print(math.acos(cos_a))
